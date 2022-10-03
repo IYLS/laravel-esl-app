ab1944bca8b1fcf43d9cf70033515769a222cdf8
Benjamin Cáceres <benjamin.caceres.ra@gmail.com>
2022-10-02 22:29:42 -0300

    Fix sections responsive issue

--- resources/views/exercises/index.blade.php
+++ resources/views/exercises/index.blade.php
@@ -34,7 +34,7 @@
                         <td class="col-2 d-none d-md-table-cell">
                             {{ $exercise->exerciseType->name }}
                         </td>
-                        <td class="col-2">
+                        <td class="col-1">
                             <div class="d-flex">
                                 <form action="{{ route("exercises.destroy", [$exercise->section->unit_id, $exercise->exercise_type_id, $exercise->id]) }}" method="POST">
                                     @method('DELETE')
@@ -56,50 +56,64 @@
             </tbody>
         </table>
 
-
         <div class="row">
-            <button class="btn btn-primary btn-sm col-12 col-md-4 me-2 mt-2" data-bs-toggle="collapse" href="#collapsableAddExercise" role="button" aria-expanded="false" aria-controls="collapsableAddExercise">Add exercise <i class="mdi mdi-arrow-down"></i></button>
-            <div class="collapse" id="collapsableAddExercise">
-                @forelse($types as $type)
-                    <div class="">
-                        <button type="button" id="add_{{ $type->underscore_name }}_button" class="btn btn-sm btn-primary mt-1" data-bs-toggle="modal" data-bs-target="#add_{{ $type->underscore_name}}_exercise_modal">
-                            {{ $type->name }}
+            <div class="col-6 d-flex justify-content-center">
+                <button class="btn btn-primary btn-sm col-12 col-md-4 mt-2" data-bs-toggle="collapse" href="#collapsableAddExercise{{ $section->id }}" role="button" aria-expanded="false" aria-controls="collapsableAddExercise{{ $section->id }}">Add exercise <i class="mdi mdi-arrow-down"></i></button>
+            </div>
+
+            <div class="col-6 d-flex justify-content-center">
+                <button class="btn btn-success btn-sm col-12 col-md-4 mt-2" data-bs-toggle="collapse" href="#collapsableAddMetacognition{{ $section->id }}" role="button" aria-expanded="false" aria-controls="collapsableAddMetacognition{{ $section->id }}">Add metacognition  <i class="mdi mdi-arrow-down"></i></button>
+            </div>
+
+            <div class="col-6 d-flex justify-content-center">
+                <div class="collapse" id="collapsableAddExercise{{ $section->id }}">
+                    @forelse($types as $type)
+                        <div class="">
+                            <button type="button" id="add_{{ $type->underscore_name }}_button" class="btn btn-sm btn-primary mt-1" data-bs-toggle="modal" data-bs-target="#add_{{ $type->underscore_name}}_exercise_modal">
+                                {{ $type->name }}
+                            </button>
+                            @include('exercises.modals.exercise_modal', ['section' => $exercise->section, 'type' => $type])
+                        </div>
+                    @empty
+                    @endforelse
+                </div>
+            </div>
+
+            <div class="col-6 d-flex justify-content-center">
+                <div class="collapse" id="collapsableAddMetacognition{{ $section->id }}">
+                    <div class="ms-1 me-1">
+                        <button type="button" id="add_multiple_choice_button" class="btn btn-sm btn-success mt-1" data-bs-toggle="modal" data-bs-target="#add_meta_multiple_choice_exercise_modal">
+                            Multiple choice
                         </button>
-                        @include('exercises.modals.exercise_modal', ['section' => $exercise->section, 'type' => $type])
+                        @include('exercises.modals.metacognition', ['section' => $section, 'underscore_type' => 'multiple_choice', 'type' => 'Multiple Choice'])
                     </div>
-                @empty
-                @endforelse
-            </div>
-
-            <button class="btn btn-success btn-sm col-12 col-md-4 me-2 mt-2" data-bs-toggle="collapse" href="#collapsableAddMetacognition" role="button" aria-expanded="false" aria-controls="collapsableAddMetacognition">Add metacognition  <i class="mdi mdi-arrow-down"></i></button>
-            <div class="collapse" id="collapsableAddMetacognition">
-                <div class="ms-1 me-1">
-                    <button type="button" id="add_multiple_choice_button" class="btn btn-sm btn-success mt-1" data-bs-toggle="modal" data-bs-target="#add_meta_multiple_choice_exercise_modal">
-                        Multiple choice
-                    </button>
-                    @include('exercises.modals.metacognition', ['section' => $section, 'underscore_type' => 'multiple_choice', 'type' => 'Multiple Choice'])
-                </div>
-                <div class="ms-1 me-1">
-                    <button type="button" id="add_drag_and_drop_button" class="btn btn-sm btn-success mt-1" data-bs-toggle="modal" data-bs-target="#add_meta_drag_and_drop_exercise_modal">
-                        Drag and Drop
-                    </button>
-                    @include('exercises.modals.metacognition', ['section' => $section, 'underscore_type' => 'drag_and_drop', 'type' => 'Drag and Drop'])
-                </div>
-                <div class="ms-1 me-1">
-                    <button type="button" id="add_open_ended_button" class="btn btn-sm btn-success mt-1" data-bs-toggle="modal" data-bs-target="#add_meta_open_ended_exercise_modal">
-                        Open-ended
-                    </button>
-                    @include('exercises.modals.metacognition', ['section' => $section, 'underscore_type' => 'open_ended', 'type' => 'Open Ended'])
-                </div>
-                <div class="ms-1 me-1">
-                    <button type="button" id="add_form_button" class="btn btn-sm btn-success mt-1" data-bs-toggle="modal" data-bs-target="#add_meta_form_exercise_modal">
-                        Form
-                    </button>
-                    @include('exercises.modals.metacognition', ['section' => $section, 'underscore_type' => 'form', 'type' => 'Form'])
+                    <div class="ms-1 me-1">
+                        <button type="button" id="add_drag_and_drop_button" class="btn btn-sm btn-success mt-1" data-bs-toggle="modal" data-bs-target="#add_meta_drag_and_drop_exercise_modal">
+                            Drag and Drop
+                        </button>
+                        @include('exercises.modals.metacognition', ['section' => $section, 'underscore_type' => 'drag_and_drop', 'type' => 'Drag and Drop'])
+                    </div>
+                    <div class="ms-1 me-1">
+                        <button type="button" id="add_open_ended_button" class="btn btn-sm btn-success mt-1" data-bs-toggle="modal" data-bs-target="#add_meta_open_ended_exercise_modal">
+                            Open-ended
+                        </button>
+                        @include('exercises.modals.metacognition', ['section' => $section, 'underscore_type' => 'open_ended', 'type' => 'Open Ended'])
+                    </div>
+                    <div class="ms-1 me-1">
+                        <button type="button" id="add_form_button" class="btn btn-sm btn-success mt-1" data-bs-toggle="modal" data-bs-target="#add_meta_form_exercise_modal">
+                            Form
+                        </button>
+                        @include('exercises.modals.metacognition', ['section' => $section, 'underscore_type' => 'form', 'type' => 'Form'])
+                    </div>
                 </div>
             </div>
-
         </div>
+
+
+
+
+
+
 
     </div>
 
